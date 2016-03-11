64d278198736545714d2e2492584198d63f94a1d
Artem Khamzin <akhamzin90@yandex.ru>
2016-03-11 19:05:38 +0300

    Home and About pages 1st iteration done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MyStories::Application.routes.draw do
+  #get "about/about" 
+  get "home_page/home"
+
+
+  root 'home_page#home'
+  match '/about', to: 'about#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
