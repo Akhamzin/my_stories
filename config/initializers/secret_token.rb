468940169976e112932534d4fa8a7e4bc645ca82
Artem Khamzin <akhamzin90@yandex.ru>
2016-03-11 13:36:24 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyStories::Application.config.secret_key_base = '193717930c1e5885b715ff2544b5d1eec3777389bebef00ce6fb617eeba55e313aed1c1e3115c70113518a69e2d99ba5673a2069700ec35e4fcf09400b6083d8'
